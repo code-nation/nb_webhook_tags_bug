2bc2e249619f9db5aba5f270e003fb55f9f1496a
Corey M <corey@codenation.com>
2020-10-04 10:23:04 +1100

    Order webhooks chronologically (most recent first)

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def home
-    @webhooks = PersonWebhook.has_missing_tags.limit(200)
+    @webhooks = PersonWebhook.has_missing_tags.limit(200).order(created_at: :desc)
     # Explicitly load records to avoid multiple database calls in the views
     # when using .any? to check if records exist before iterating over them
     @webhooks.load
