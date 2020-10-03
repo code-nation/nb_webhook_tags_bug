b235e8db25f0fa32db7f4afd82bbf122ab3cad7f
Corey M <corey@codenation.com>
2020-10-03 20:33:34 +1000

    Fix foolish syntax error in migration file

--- db/migrate/20201003102500_create_person_webhooks.rb
+++ db/migrate/20201003102500_create_person_webhooks.rb
@@ -2,9 +2,9 @@ class CreatePersonWebhooks < ActiveRecord::Migration[6.0]
   def change
     create_table :person_webhooks do |t|
       t.bigint :nb_signup_id
-      t.string :tags, :string, array: true, default: '{}'
-      t.string :actual_latest_tags, :string, array: true, default: '{}'
-      t.string :missing_tags, :string, array: true, default: '{}'
+      t.string :tags, array: true, default: '{}'
+      t.string :actual_latest_tags, array: true, default: '{}'
+      t.string :missing_tags, array: true, default: '{}'
       t.boolean :has_missing_tags, null: false, default: false
 
       t.timestamps
